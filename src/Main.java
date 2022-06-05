7b68e3846fc39a2d7180ba8251e5a8ab961b02da
Victoria Rouser <vic86rous@gmail.com>
2022-06-05 17:56:57 -0500

    adjusted some components

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello! What is you name?");
-
-        System.out.println("Well, " + input + "I am thinking of a number between 1 and 20.");
-        System.out.println();
         Scanner input = new Scanner(System.in);
-        int intro = input.nextInt();
+        input.nextInt();
+        System.out.println("Well, " + input + "I am thinking of a number between 1 and 20.");
+//        Scanner input = new Scanner(System.in);
+        input.nextInt();
+
 
 
         Random rand = new Random();
