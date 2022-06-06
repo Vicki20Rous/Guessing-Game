5dc6503644766bf351789a82b5b09c2048b0ce48
Victoria Rouser <vic86rous@gmail.com>
2022-06-06 12:24:02 -0500

    reorganized functions

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,36 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-
+//        String username = "";
         Random rand = new Random();
         Scanner in = new Scanner(System.in);
 
+//        System.out.println("Welcome to Number Guess!!! \n" + " Hello!! What is your name ");
+//        int username = in.nextInt();
+
         int number = rand.nextInt(20) + 1;
-        System.out.println("Random Number is " + number);
 
-        System.out.println("Welcome to Number Guess!!! \n" + "I am thinking of a number between 1 and 20.\n" + "Take a guess..");
-        int guess = in.nextInt();
 
-        int tryAttempt = 0;
+//        int input = in.nextInt();
 
-            if(number == guess) {
+        while (true) {
+            System.out.println("Welcome to Number Guess!!! \n" +"Well, I am thinking of a number between 1 and 20.");
+            System.out.println( "Take a guess..");
+            int guess = in.nextInt();
+
+            int tryAttempt = 0;
+
+            if (number == guess) {
                 System.out.println("Good job!!! You guessed my number in guesses!!");
-            }
-
-            else if(number > guess) {
+                break;
+            } else if (number < guess) {
                 System.out.println("Your guess is too high.");
 
-            }
-            else if (number < guess){
+            } else {
                 System.out.println("Your guess is too low.");
             }
 
 
-
+        }
     }
 }
