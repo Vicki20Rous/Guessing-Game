6e9e63e8672ef78ab4a066db48bfca6e29dd4f03
Victoria Rouser <vic86rous@gmail.com>
2022-06-08 10:43:42 -0500

    made some adjustments to code

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,33 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-//        String username = "";
+        String username = "";
         Random rand = new Random();
         Scanner in = new Scanner(System.in);
-
-//        System.out.println("Welcome to Number Guess!!! \n" + " Hello!! What is your name ");
-//        int username = in.nextInt();
-
         int number = rand.nextInt(20) + 1;
+        int tryAttempt = 0;
+        String playAgain = "";
+
+
+        System.out.println("Welcome to Number Guess!!! \n" + " Hello!! What is your name " + username);
+        System.out.println("Well, I am thinking of a number between 1 and 20.");
+//        String input = Integer.toString(Integer.parseInt(username));
+
 
 
 //        int input = in.nextInt();
-        int tryAttempt = 0;
         while (true) {
-            System.out.println("Welcome to Number Guess!!! \n" +"Well, I am thinking of a number between 1 and 20.");
             System.out.println( "Take a guess..");
             int guess = in.nextInt();
             tryAttempt++;
 
             if (number == guess) {
-                System.out.println("Good job!!! You guessed my number in " + tryAttempt + " guesses!!");
+                System.out.println("Good job!!! You guessed my number in " + tryAttempt + " guesses!!" + "\nWould you like to play again? (yes or no)" + playAgain);
                 break;
             } else if (number < guess) {
                 System.out.println("Your guess is too high.");
 
-            } else {
+            } else if (number > guess) {
                 System.out.println("Your guess is too low.");
             }
 
